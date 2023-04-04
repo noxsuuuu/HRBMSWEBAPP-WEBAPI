785fd4a4350af48bef5a8ca64c7f6588d107de95
ivhanpatrick21 <ivhan.deleon00@gmail.com>
2023-04-05 06:59:10 +0800

    etits ko malaki

--- HRBMSWEBAPP/Repository/Database/BookingDBRepository.cs
+++ HRBMSWEBAPP/Repository/Database/BookingDBRepository.cs
@@ -12,70 +12,106 @@ namespace HRBMSWEBAPP.Repository.Database
         {
             _context = context;
         }
-        public Booking AddBooking(Booking booking)
+        //public Booking AddBooking(Booking booking)
+        //{
+
+        //    _context.Add(booking);
+        //    _context.SaveChanges();
+
+        //    return booking; 
+        //}
+        public Task AddBooking(Booking booking)
         {
-
-            _context.Add(booking);
-            _context.SaveChanges();
-
-            return booking; 
+            this._context.Add(booking);
+            return this._context.SaveChangesAsync();
         }
 
-        public Booking DeleteBooking(int bookingId)
+        public Task DeleteBooking(int bookingId)
         {
-            /*var booking = this._context.Booking.FindAsync(bookingId);
+            var booking = this._context.Booking.FindAsync(bookingId);
             if (booking.Result != null)
             {
                 this._context.Booking.Remove(booking.Result);
             }
 
-            return this._context.SaveChangesAsync();*/
+            return this._context.SaveChangesAsync();
+        }
+        //public Booking DeleteBooking(int bookingId)
+        //{
+        //    /*var booking = this._context.Booking.FindAsync(bookingId);
+        //    if (booking.Result != null)
+        //    {
+        //        this._context.Booking.Remove(booking.Result);
+        //    }
 
-            var booking = GetBookingById(bookingId);
-            if(booking != null) 
-            {
-                _context.Booking.Remove(booking);
-                _context.SaveChanges();
+        //    return this._context.SaveChangesAsync();*/
+
+        //    var booking = GetBookingById(bookingId);
+        //    if(booking != null) 
+        //    {
+        //        _context.Booking.Remove(booking);
+        //        _context.SaveChanges();
             
+        //    }
+        //    return booking;
+
+        //}
+
+        //public Booking UpdateBooking(int bookingId, Booking booking)
+        //{
+        //    /*this._context.Update(booking);
+        //    return this._context.SaveChangesAsync();*/
+
+        //    _context.Booking.Update(booking);
+        //    _context.SaveChanges();
+
+        //    return booking;
+
+
+        //}
+        public Task UpdateBooking(int bookingId, Booking booking)
+        {
+            this._context.Update(booking);
+            return this._context.SaveChangesAsync();
+        }
+        public Task<List<Booking>> GetAllBooking()
+        {
+            return this._context.Booking.ToListAsync();
+        }
+
+        public Task<Booking> GetBookingById(int booking_id)
+        {
+            var booking = this._context.Booking
+                .FirstOrDefaultAsync(m => m.Id == booking_id);
+
+            if (booking == null)
+            {
+                return null;
             }
-            return booking;
-
-        }
-
-        public Booking UpdateBooking(int bookingId, Booking booking)
-        {
-            /*this._context.Update(booking);
-            return this._context.SaveChangesAsync();*/
-
-            _context.Booking.Update(booking);
-            _context.SaveChanges();
 
             return booking;
-
-
         }
+        //public List<Booking> GetAllBooking()
+        //{
+        //    return _context.Booking.Include(e => e.Room).ToList();
+        //}
 
-        public List<Booking> GetAllBooking()
-        {
-            return _context.Booking.Include(e => e.Room).ToList();
-        }
+        //public Booking GetBookingById(int booking_id)
+        //{
+        //    /*var booking = _context.Booking
+        //           *//* .Include(e => e.User)
+        //            *//*.Include(e => e.Room)
+        //            .FirstOrDefault(m => m.Id == booking_id);
 
-        public Booking GetBookingById(int booking_id)
-        {
-            /*var booking = _context.Booking
-                   *//* .Include(e => e.User)
-                    *//*.Include(e => e.Room)
-                    .FirstOrDefault(m => m.Id == booking_id);
+        //     if (booking == null)
+        //     {
+        //         return null;
+        //     }
 
-             if (booking == null)
-             {
-                 return null;
-             }
+        //     return booking;*/
 
-             return booking;*/
+        //    return _context.Booking.AsNoTracking().FirstOrDefault(t => t.Id == booking_id);
 
-            return _context.Booking.AsNoTracking().FirstOrDefault(t => t.Id == booking_id);
-
-        }
+        //}
     }
 }
