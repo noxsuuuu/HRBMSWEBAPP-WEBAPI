c51456f6cf77f97726f8cb51e2aead8fc9959614
ivhanpatrick21 <ivhan.deleon00@gmail.com>
2023-04-25 02:34:26 +0800

    Unique Booking

--- HRBMSWEBAPP/Validations/UniqueBooking.cs
+++ HRBMSWEBAPP/Validations/UniqueBooking.cs
@@ -11,14 +11,6 @@ namespace HRBMSWEBAPP.Validations
             var dbContext = (HRBMSDBCONTEXT)validationContext.GetService(typeof(HRBMSDBCONTEXT));
             var bookid = value;
 
-            // Check if the roomId already exists in the database
-            //var existingRoom = dbContext.Room.FirstOrDefault(r => r.Id == roomId);
-            //var existingFloor = dbContext.Room.FirstOrDefault(r => r.Floor_Number == roomId);
-            //if (existingRoom != null || existingFloor != null)
-            //{
-            //    return new ValidationResult("The Room Number already exists.");
-            //}
-
             var room = (Booking)validationContext.ObjectInstance;
             var existingbook = dbContext.Booking.FirstOrDefault(b => room.Id == room.Id && b.CheckIn == room.CheckIn && b.CheckIn == room.CheckIn);
             if (existingbook != null)
